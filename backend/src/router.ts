300f40ac50b6c67cf15850a42771458dd284b826
Xxxhite <1277229128@qq.com>
2026-06-16 02:03:40 +0800

    feat(backend): 添加 commentController 到 apiRouter

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -2,8 +2,10 @@ import {Elysia} from "elysia";
 import {categoryController} from "./controllers/category.controller.ts";
 import {postController} from "./controllers/post.controller.ts";
 import {tagController} from "./controllers/tag.controller.ts";
+import {commentController} from "./controllers/comment.controller.ts";
 
 export const apiRouter = new Elysia({prefix: "/api"})
     .use(categoryController)
     .use(postController)
-    .use(tagController);
+    .use(tagController)
+    .use(commentController);
