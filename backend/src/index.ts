4b69fbed407b5d77890c08e66aa39b2e36d01ef1
Xxxhite <1277229128@qq.com>
2026-06-16 00:30:15 +0800

    feat: integrate category controller and update TypeScript configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import { Elysia } from "elysia";
 import { cors } from '@elysiajs/cors'
+import { categoryController } from "./controllers/category.controller";
 
 const app = new Elysia()
     .use(cors())
     .get("/", () => "Hello Elysia")
+    .use(categoryController)
     .listen(5199);
 
 console.log(
