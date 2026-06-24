d05d5222b583cc80e81ec57920776d6f887181b7
Xxxhite <1277229128@qq.com>
2026-06-24 23:49:57 +0800

    chore(content): sync main@ceb364f

--- frontend/src/config/siteConfig.ts
+++ frontend/src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 // 定义站点时区
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 
-export const siteConfig: SiteConfig = {
+export const siteConfig: SiteConfig = { // TODO: 反正todo
     title: "Mizuki",
     subtitle: "One demo website",
     siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
