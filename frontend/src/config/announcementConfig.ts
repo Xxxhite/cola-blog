5d31c0a3c1fbc3b42c042b0cdfb1e350e6bab66a
Xxxhite <1277229128@qq.com>
2026-06-25 01:38:49 +0800

    feat: 修改公告渲染逻辑，支持在配置中使用html标签

--- frontend/src/config/announcementConfig.ts
+++ frontend/src/config/announcementConfig.ts
@@ -3,10 +3,10 @@ import type { AnnouncementConfig } from "../types/config";
 // 公告栏配置
 export const announcementConfig: AnnouncementConfig = {
 	title: "", // 公告标题，填空使用i18n字符串Key.announcement
-	content: "绝赞建设中...", // 公告内容
+	content: "绝赞建设中...<br>测试换行", // 公告内容    // 支持html标签
 	closable: false, // 允许用户关闭公告
 	link: {
-		enable: true, // 启用链接
+		enable: false, // 启用链接
 		text: "查看更多", // 链接文本
 		url: "/about/", // 链接 URL
 		external: false, // 内部链接
