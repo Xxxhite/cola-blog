6ede103933f21e898e0c8566fab61c932db9d053
Xxxhite <1277229128@qq.com>
2026-06-25 01:26:17 +0800

    feat: 优化页脚内容表现

--- frontend/src/config/footerConfig.ts
+++ frontend/src/config/footerConfig.ts
@@ -8,7 +8,8 @@ export const footerConfig: FooterConfig = {
         "<img src=\"/assets/registration-icon/icon.png\" style=\"width: 12px; height: 12px; vertical-align: center; margin-bottom: 3px;\" alt=\"icon\">" +
         "<a href=\"https://beian.mps.gov.cn/#/query/webSearch?code=33019202003078\" rel=\"noreferrer\" target=\"_blank\">浙公网安备33019202003078号</a>" +
         "<a href=\"https://beian.miit.gov.cn/#/Integrated/index\" rel=\"noreferrer\" target=\"_blank\">浙ICP备2026043084号-1</a>" +
-        "</div>", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+        "</div>",
+    // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
