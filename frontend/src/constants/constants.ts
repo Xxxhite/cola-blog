49a39b2d4a2dadaeedaf932299cdc2aec344c8b5
Xxxhite <1277229128@qq.com>
2026-06-26 05:52:57 +0800

    fix: 修改页面宽度

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -15,7 +15,7 @@ export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
 
 // Page width: rem
-export const PAGE_WIDTH = 125;
+export const PAGE_WIDTH = 117;
 
 // Category constants
 export const UNCATEGORIZED = "uncategorized";
