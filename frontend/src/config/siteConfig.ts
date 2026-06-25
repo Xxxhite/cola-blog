5547f892882f78172aec762706558a68f27a8270
Xxxhite <1277229128@qq.com>
2026-06-25 17:16:57 +0800

    chore(content): sync main@da5eb8a

--- frontend/src/config/siteConfig.ts
+++ frontend/src/config/siteConfig.ts
@@ -7,10 +7,10 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 
 export const siteConfig: SiteConfig = { // TODO: 反正todo
-    title: "Mizuki",
+    title: "一罐可乐",
     subtitle: "One demo website",
     siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-    siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+    siteStartDate: "2026-06-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
     timeZone: SITE_TIMEZONE,
 
