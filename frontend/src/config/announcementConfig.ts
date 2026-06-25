04e932bd4ef99c74c44cb29b748a6267179280c2
Xxxhite <1277229128@qq.com>
2026-06-26 05:54:58 +0800

    chore: 修改公告内容

--- frontend/src/config/announcementConfig.ts
+++ frontend/src/config/announcementConfig.ts
@@ -3,7 +3,8 @@ import type { AnnouncementConfig } from "../types/config";
 // 公告栏配置
 export const announcementConfig: AnnouncementConfig = {
 	title: "", // 公告标题，填空使用i18n字符串Key.announcement
-	content: "绝赞建设中...<br>测试换行", // 公告内容    // 支持html标签
+	content: // 公告内容，支持html标签
+        "绝赞建设中...可能马上就搞完了<br>测试换行",
 	closable: false, // 允许用户关闭公告
 	link: {
 		enable: false, // 启用链接
