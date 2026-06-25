769ebbd4e2fd624756b102e47c1607860822433b
Xxxhite <1277229128@qq.com>
2026-06-26 07:53:13 +0800

    chore: 换了个头像

--- frontend/src/config/profileConfig.ts
+++ frontend/src/config/profileConfig.ts
@@ -2,7 +2,7 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Xxxhite",
 	bio: "早点睡觉",
 	typewriter: {
