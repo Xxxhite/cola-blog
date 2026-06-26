893ccab9ed95e22aa3fcecefd28e87dc854c127a
Xxxhite <1277229128@qq.com>
2026-06-27 06:23:03 +0800

    chore:

--- frontend/src/config/siteConfig.ts
+++ frontend/src/config/siteConfig.ts
@@ -8,7 +8,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 
 export const siteConfig: SiteConfig = {
     title: "一罐可乐",
-    subtitle: "我没想好写啥",
+    subtitle: "随便写点什么",
     siteURL: "https://blog.sopak.space/", // 请替换为你的站点URL，以斜杠结尾
     siteStartDate: "2026-05-28", // 站点开始运行日期，用于站点统计组件计算运行天数
 
